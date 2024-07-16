ac133b3e0a2f897e2042947d86d456db89743e3d
Sacha Darras <sachdarras@gmail.com>
2024-07-16 20:38:01 +0200

    caroussel

--- client/src/pages/Acceuil.jsx
+++ client/src/pages/Acceuil.jsx
@@ -1,12 +1,17 @@
+import Caroussel from "../components/Carousseldynamiqueacceuil";
+
 function Acceuil() {
   return (
-    <div className="header-container">
-      <h1>Découvrez le Monde du Chien</h1>;
-      <p>
-        Bienvenue dans l'univers des chiens où l'aventure et la compagnie fidèle
-        vous attendent.
-      </p>
-    </div>
+    <>
+      <div className="header-container">
+        <h1>Découvrez le Monde du Chien</h1>;
+        <p>
+          Bienvenue dans l'univers des chiens où l'aventure et la compagnie
+          fidèle vous attendent.
+        </p>
+      </div>
+      <Caroussel />
+    </>
   );
 }
 
