0a519924cd9f5bbe8ef7f309ef5ae91614e4a32d
Sacha Darras <sachdarras@gmail.com>
2024-07-18 11:06:54 +0200

    pointage sur la carte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./main.scss";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <AuthProvider>
         <Header />
         <Outlet />
+        <Footer />
       </AuthProvider>
     </div>
   );
